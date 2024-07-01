e986be491d0f66f1c207423a084328d2cb51df1e
hikaru1309 <hikarishiro2001@icloud.com>
2024-07-02 00:01:03 +0900

    clear: station4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 // DO NOT DELETE
-
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [ dogUrl, setDogUrl ] = useState("https://images.dog.ceo/breeds/pug/n02110958_14768.jpg");
+
+
   return (
     <div>
-      <header>
-        <h1>Dogアプリ</h1>
-      </header>
-      <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/pug/n02110958_14768.jpg" alt="" />
+      <div>
+        <header>
+          <h1>Dogアプリ</h1>
+        </header>
+        <p>犬の画像を表示するサイトです</p>
+        <img src={dogUrl} alt="" />
+      </div>
+      <button>更新</button>
     </div>
   )
 }
